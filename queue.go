9505a7107d2ea1beaa3984943522a962bc65583f
Antony Holmes <antony@antonyholmes.dev>
2025-09-19 17:48:23 -0400

    Fixed: Fixed email templates not rendering data

--- queue.go
+++ queue.go
@@ -1,24 +1,16 @@
 package mailserver
 
-const (
-	QUEUE_EMAIL_TYPE_VERIFY           = "verify"
-	QUEUE_EMAIL_TYPE_VERIFIED         = "verified"
-	QUEUE_EMAIL_TYPE_PASSWORDLESS     = "passwordless"
-	QUEUE_EMAIL_TYPE_PASSWORD_RESET   = "password-reset"
-	QUEUE_EMAIL_TYPE_PASSWORD_UPDATED = "password-updated"
-	QUEUE_EMAIL_TYPE_EMAIL_RESET      = "email-reset"
-	QUEUE_EMAIL_TYPE_EMAIL_UPDATED    = "email-updated"
-	QUEUE_EMAIL_TYPE_ACCOUNT_CREATED  = "account-created"
-	QUEUE_EMAIL_TYPE_ACCOUNT_UPDATED  = "account-updated"
-	QUEUE_EMAIL_TYPE_OTP              = "otp"
-)
+type Payload struct {
+	DataType string `json:"dataType"`
+	Data     string `json:"data"`
+}
 
 type MailItem struct {
-	Name      string `json:"name"`
-	To        string `json:"to"`
-	EmailType string `json:"type"`
-	TTL       string `json:"ttl"`
-	Token     string `json:"token"`
+	Name      string   `json:"name"`
+	To        string   `json:"to"`
+	EmailType string   `json:"type"`
+	TTL       string   `json:"ttl"`
+	Payload   *Payload `json:"payload,omitempty"`
 	//RedirectUrl string `json:"redirectUrl"`
 	LinkUrl string `json:"linkUrl"`
 	// Used to indicate if emails should be for web users or
